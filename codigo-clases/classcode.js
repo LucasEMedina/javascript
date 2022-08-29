3a81ef16c1b4bc707c7a847d947f4c70b230b1e3
Lucas Medina <lucasmed.emme@gmail.com>
2022-08-28 21:26:46 -0300

    se realizo el primer desafio entregable

--- codigo-clases/classcode.js
+++ codigo-clases/classcode.js
@@ -307,3 +307,169 @@ switch(color){
     alert('No ingresaste ningun color esperado')
     break
 }
+
+
+///////////     CLASE 4
+
+// // creando una funcion
+
+// function sumar(){
+// const numero1 = parseInt(prompt('Ingresa un numero'))
+// const numero2 = parseInt(prompt('Ingresa otro numero'))
+
+// const resultado = numero1 + numero2
+// alert('El resultado es '+resultado)
+// }
+
+// //sumar() // llamando a la funcion
+
+// function restar(numero1,numero2){
+//     const restando = numero1 - numero2
+//     const mitad = restando/2
+//     console.log(restando)
+//     return mitad
+// }
+// //console.log(restando)
+// const resultadoResta = restar(10,5)
+// console.log(resultadoResta)
+// console.log(restando)
+// //alert('el resultado es: '+resultadoResta)
+
+// const variableGlobal = 2
+
+
+// function mayorA10(num1, num2) {
+// let variableLocal = 10
+//   const resultado = num1 + num2 + variableGlobal
+//   if (resultado > 10) {
+//     return 'mayor a 10'
+//   } else {
+//     return 'menor o igual a 10'
+//   }
+// }
+
+
+// const resultadoMayorA10 = mayorA10(4, 5)
+// console.log('el resultado es ' + resultadoMayorA10)
+
+function esVocal(letra) {
+    console.log(variableLocal)
+  const letraMinuscala = letra.toLowerCase()
+  if (
+    letraMinuscala === 'a' ||
+    letraMinuscala === 'e' ||
+    letraMinuscala === 'i' ||
+    letraMinuscala === 'o' ||
+    letraMinuscala === 'u'
+  ) {
+    return 'es una vocal'
+  } else {
+    return 'no es una vocal'
+  }
+}
+
+// console.log(esVocal('A'))
+
+// let esVocal = function(letra){
+
+// }
+
+
+// esVocal()
+let exponenteFunction = function(num,exponenteParams){
+    return num**exponenteParams
+}
+let exponenteArrow = (num,exponenteParams) =>{
+    return num**exponenteParams
+}
+
+//let cuadrado = exponente(2)
+//let cubo = exponente(3)
+
+console.log(exponente(6,2))
+console.log(exponente(6,3))
+
+//funcion autoinvocada
+
+// +function(letra) {
+//     //console.log(variableLocal)
+//   const letraMinuscala = letra.toLowerCase()
+//   if (
+//     letraMinuscala === 'a' ||
+//     letraMinuscala === 'e' ||
+//     letraMinuscala === 'i' ||
+//     letraMinuscala === 'o' ||
+//     letraMinuscala === 'u'
+//   ) {
+//     return 'es una vocal'
+//   } else {
+//     return 'no es una vocal'
+//   }
+// }('A')
+
+
+//// EJEMPLO PARA EL SEGUNDO DESAFIO ////
+
+let totalCompra = 0
+let productoSeleccionado = parseInt(
+  prompt(
+    'Ingresa el numero del producto que quieres comprar: 1.Iphone - 2.TV - 3.Ipad - 4.Computador'
+  )
+)
+let seguirComprando = true
+let decision
+
+while (seguirComprando === true) {
+  if (productoSeleccionado === 1) {
+    totalCompra = totalCompra + 650
+  } else if (productoSeleccionado === 2) {
+    totalCompra = totalCompra + 350
+  } else if (productoSeleccionado === 3) {
+    totalCompra = totalCompra + 250
+  } else if (productoSeleccionado === 4) {
+    totalCompra = totalCompra + 1200
+  } else{
+    productoSeleccionado = parseInt(prompt('ingresa un producto existente: 1.Iphone - 2.TV - 3.Ipad - 4.Computador'))
+    continue
+  }
+
+  decision = parseInt(prompt('Quieres seguir agregando productos? 1.Si - 2.No'))
+  if (decision === 1) {
+    productoSeleccionado = parseInt(
+      prompt(
+        'Ingresa el numero del producto que quieres comprar: 1.Iphone - 2.TV - 3.Ipad - 4.Computador'
+      )
+    )
+  } else if (decision === 2) {
+    seguirComprando = false
+  }
+}
+
+alert('El valor total sin descuento ni impuestos es: '+totalCompra)
+
+function calcularPrecioConDescuento(valor){
+let descuento = 0
+if(valor<=1000){
+    descuento = 10
+} else if(valor>1000 && valor<=1500){
+    descuento = 15
+} else if(valor>1500 && valor<=2000){
+    descuento = 20
+} else {
+    descuento = 25
+}
+let valorDescuento = valor *(descuento/100)
+valor = valor - valorDescuento
+return valor
+}
+let valorConDescuento = calcularPrecioConDescuento(totalCompra)
+alert('El total con descuento sin impuestos es: '+valorConDescuento)
+
+function calcularPrecioConImpuesto(valor){
+    const impuestos = valor * (8/100)
+    return valor + impuestos
+}
+
+let valorFinalConImpuesto = calcularPrecioConImpuesto(valorConDescuento)
+alert('El valor final a pagar es de: '+valorFinalConImpuesto)
+
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let bostero = prompt ("Hola, sos hincha de Boca??")
+/// PRIMER DESAFIO COMPLEMENTARIO
+
+/* let bostero = prompt ("Hola, sos hincha de Boca??")
 
 if (bostero.toLowerCase() == "si") {
 
@@ -59,4 +61,73 @@ while(defensa.toLowerCase()!=='fabra'){
     defensa = prompt("Error, Intentalo de nuevo")
 }
 
-alert("Exacto, "+defensa +" tiene 15 goles en Boca!!")
+alert("Exacto, "+defensa +" tiene 15 goles en Boca!!") */
+
+
+
+/// PRIMER DESAFIO ENTREGABLE
+
+
+let nombreEquipo = prompt("Ingresa el nombre de tu equipo!!")
+let cantidadFechas = parseInt (prompt("Cuantas fechas tiene el campeonato??"))
+
+let totalPuntos = 0
+let fechajugada = true
+let jugaron
+let fecha = 1
+let resultadoPartido = prompt('Ingresa el resultado de la fecha '+fecha +': G ganado - E empatado - P perdido')
+
+while (fechajugada = true, fecha<cantidadFechas) {
+    fecha = fecha + 1
+
+    if (resultadoPartido.toUpperCase() === "G") {
+        totalPuntos = totalPuntos + 3        
+    }
+    else if (resultadoPartido.toUpperCase() === "E") {
+        totalPuntos = totalPuntos + 1
+    }
+    else if (resultadoPartido.toUpperCase() === "P"){
+        totalPuntos = totalPuntos + 0
+    }
+    else{resultadoPartido =
+        prompt(
+            'Ingresa un resultado valido: G ganado - E empatado - P perdido'
+        ) 
+        fecha = fecha - 1
+        continue
+    }
+
+    jugaron = parseInt(prompt("Jugaron la fecha "+fecha +"? 1.Si - 2.No"))
+    
+         
+    if (jugaron === 1){
+        resultadoPartido = prompt('Ingresa el resultado del partido jugado: G ganado - E empatado - P perdido')
+    }
+    else if (jugaron === 2){
+        fechajugada = false
+        break
+    }
+     else {
+        break        
+    }
+      
+    }
+
+    alert ('El equipo '+nombreEquipo +' logro '+totalPuntos)
+
+    const calcularPromedio =(a, b) => {
+        let promedio = a / b
+        return promedio
+    }
+
+    let fechasJugadas = parseInt(prompt('Ingresa la cantidad de fechas jugadas.'))
+    if (fechasJugadas > cantidadFechas) {
+        fechasJugadas = parseInt(prompt('No puedes jugar mas fechas de las que tiene el campeonato. Ingresa la cantidad de fechas jugadas.'))
+    }
+
+    let promedioEquipo = calcularPromedio(totalPuntos, fechasJugadas)
+    alert ('El promedio para el descenso del '+nombreEquipo+' es de '+promedioEquipo+' !!')
+
+
+
+    
